5c6fb73a5d7d0cd74773dd39ab637822c0184c5c
Rajat Das <rajat@Rajats-MacBook-Air.local>
2023-11-13 14:32:09 +0530

    downgraded next

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  // experimental: {
+  //   serverActions: true,
+  // },
   redirects() {
     try {
       return get("redirects");
