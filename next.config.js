88f35e19f4dcf0f565af1dc806a204dc84c85922
Rajat Das <rajat@Rajats-MacBook-Air.local>
2023-11-13 14:34:05 +0530

    serverActions flag

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // experimental: {
-  //   serverActions: true,
-  // },
+  experimental: {
+    serverActions: true,
+  },
   redirects() {
     try {
       return get("redirects");
