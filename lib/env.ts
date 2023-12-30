10cde7eda3605819e98b3838e8fb48395ef75378
iRajatDas <draj123258@gmail.com>
2023-12-30 13:24:44 +0530

    added edge store env

--- lib/env.ts
+++ lib/env.ts
@@ -11,8 +11,8 @@ const envSchema = zod.object({
   NEXTAUTH_URL: zod.string(),
   GHP_PAT: zod.string(),
   RESEND_EMAIL_FROM: zod.string(),
-  // EDGE_STORE_ACCESS_KEY: zod.string(),
-  // EDGE_STORE_SECRET_KEY: zod.string(),
+  EDGE_STORE_ACCESS_KEY: zod.string(),
+  EDGE_STORE_SECRET_KEY: zod.string(),
 });
 
 const env = envSchema.parse(process.env);
