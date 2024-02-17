da67448eb9a51f8910fb23d1bdae5cbd15395e69
Rajat Das <rajat@Rajats-MacBook-Air.local>
2024-02-17 17:24:49 +0530

    feat: added static robots.txt file; added timeline to sitemap with typesafe and freq & priority

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,21 +2,48 @@ import env from "@/lib/env";
 import { getISODate } from "@/lib/utils";
 import { allBlogs, allSnippets } from "contentlayer/generated";
 
+type Sitemap = Array<{
+  url: string;
+  lastModified?: string | Date;
+  changeFrequency?:
+    | "always"
+    | "hourly"
+    | "daily"
+    | "weekly"
+    | "monthly"
+    | "yearly"
+    | "never";
+  priority?: number;
+}>;
+
 export default async function sitemap() {
   const homePage = {
     url: `${env.NEXTAUTH_URL!}`,
+    
+    // current date
+    lastModified: new Date().toISOString(),
+    changefreq: "hourly",
+    priority: 1,
+  };
+
+  const timeline = {
+    url: `${env.NEXTAUTH_URL!}/timeline`,
     lastModified: new Date("2023-11-07").toISOString(),
   };
 
   const blogs = allBlogs.map((post) => ({
     url: `${env.NEXTAUTH_URL!}/blog/${post.slug}`,
     lastModified: getISODate(post.publishedAt),
+    changefreq: "hourly",
+    priority: 0.8,
   }));
 
   const snippets = allSnippets.map((post) => ({
     url: `${env.NEXTAUTH_URL!}/snippets/${post.slug}`,
     lastModified: getISODate(post.publishedAt),
+    changefreq: "hourly",
+    priority: 0.8,
   }));
 
-  return [homePage, ...snippets, ...blogs];
+  return [homePage, timeline, ...snippets, ...blogs] satisfies Sitemap;
 }
